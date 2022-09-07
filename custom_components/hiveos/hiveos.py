0737d4901375c720caebbd8e107d5193b0e329b2
Niklas Ekman <nikl.ekman@gmail.com>
2022-09-08 00:21:23 +0200

    start/stop mining

--- custom_components/hiveos/hiveos.py
+++ custom_components/hiveos/hiveos.py
@@ -6,7 +6,7 @@ class HiveOsWorkerParams(TypedDict):
     """For easy reference of which params we use from the API"""
     unique_id: int
     name: str
-    active: bool
+    state: bool
     farm_id: int
     version: str
     farm_name: str
@@ -53,6 +53,13 @@ class HiveOsApi:
         """GET a specific worker from a farm"""
         return await self._request("get", f"farms/{farm_id}/workers/{worker_id}")
 
-    async def worker_set_active(self, farm_id: int, worker_id: int, active: bool = True):
-        """Set a worker to active or inactive"""
-        await self._request("patch", f"farms/{farm_id}/workers/{worker_id}", {"active": active})
+    async def worker_set_state(self, farm_id: int, worker_id: int, state: bool = True):
+        """Set a worker to start/stop"""
+        params = {
+            "command": "exec",
+            "data": {
+                "cmd": "miner " + ("start" if state else "stop")
+            }
+        }
+
+        await self._request("patch", f"farms/{farm_id}/workers/{worker_id}", params)
